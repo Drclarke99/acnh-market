07ee12ac035375c87a21fe35b15a6ba261d2bb33
David Clarke <drclarke99@gmail.com>
2023-06-17 19:01:30 -0700

    edited files

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,14 +6,14 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <title>Nook's Cay - Home</title>
+        <title>Home - Nook's Cay</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <Header />
       <main>
         <h1>Welcome to Nook's Cay</h1>
-        <h3>The #1 resource for your fishing needs</h3>
+        <h3>The #1 fishing market in New Horizons</h3>
         <Search />
       </main>
     </div>
